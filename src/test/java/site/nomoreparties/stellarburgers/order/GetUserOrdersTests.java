63ba653383c6bf5ebaa59ccff23236bd0e502d77
iozimniy <Kitriss@gmail.com>
2023-12-10 00:42:48 +0300

    add get orders tests

---
+++ src/test/java/site/nomoreparties/stellarburgers/order/GetUserOrdersTests.java
@@ -0,0 +1,47 @@
+package site.nomoreparties.stellarburgers.order;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import site.nomoreparties.stellarburgers.user.*;
+
+public class GetUserOrdersTests {
+
+    private final OrderClient client = new OrderClient();
+    private final OrderChecks checks = new OrderChecks();
+    private User user;
+    private String accessToken;
+
+    @Before
+    public void setAndAuthUser() {
+        user = UserGenerator.generateRandomUser();
+        ValidatableResponse response = UserClient.createUser(user);
+        accessToken = UserChecks.assertUserCreateSuccessfully(response, user.getEmail(), user.getName());
+
+        var authdata = AuthData.from(user);
+        UserClient.loginUser(authdata);
+
+        var order = OrderGenerator.orderWithIngredient();
+        ValidatableResponse orderWithIngredient = client.createOrderWithAuth(order, accessToken);
+    }
+
+    @Test
+    public void getUsersOrdersWithAuth() {
+        ValidatableResponse getUsersOrders = client.getUsersOrdersWithAuth(accessToken);
+        checks.getUsersOrdersSuccessfully(getUsersOrders);
+    }
+
+    @Test
+    public void getUsersOrdersWithoutAuth() {
+        ValidatableResponse getUsersOrders = client.getUsersOrdersWithoutAuth();
+        checks.getUsersOrdersWithoutAuth(getUsersOrders);
+    }
+
+    @After
+    public void deleteUser() {
+        ValidatableResponse deleteUser = UserClient.delete(accessToken);
+        UserChecks.assertUserDeleteSuccsessfully(deleteUser);
+    }
+
+}
